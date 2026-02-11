a9071dd84c1e65eae9dcca8ce7483afbc3892c41
bin <jeongbin@bagjeongbin-ui-MacBookPro.local>
2026-02-11 20:28:18 +0900

    :first commit

---
+++ src/main/java/com/xmpy/demo/XmpyApplication.java
@@ -0,0 +1,13 @@
+package com.xmpy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xmpy/demo/XmpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmpy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
