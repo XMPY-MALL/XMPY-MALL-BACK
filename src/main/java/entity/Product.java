71ea0559904e7ad00c0bf2e6112fb139b0df1464
bin <jeongbin@bagjeongbin-ui-MacBookPro.local>
2026-02-13 21:51:53 +0900

    add: add

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 public class Product {
     private long productId;
     private long categoryId;
+    private long categoryDetailId;
     private String productName;
     private String description;
     private boolean isBest;
