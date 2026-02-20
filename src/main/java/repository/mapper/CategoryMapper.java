12b7c4ca6346629940ad87fbb7ff73c1af2c2ec4
bin <jeongbin@bagjeongbin-ui-MacBookPro.local>
2026-02-20 18:50:39 +0900

    add: mapper

---
+++ src/main/java/repository/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package repository.mapper;
+
+import entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    // 카테고리 디테일 전체조회 (상의 하의 하의 안 카테고리)
+    List<Category> findAll();
+
+
+    // 디테일 Id로 조회해서 수정
+
+    Category findByCategoryDetailId(@Param("categoryDetailId") long categoryDetailId);
+}
--- src/main/java/repository/mapper/ProductMapper.java
+++ src/main/java/repository/mapper/ProductMapper.java
@@ -1,9 +1,38 @@
 package repository.mapper;
 
+import entity.Product;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ProductMapper {
+
+    // 상품 전체 조회
+    List<Product> findAll();
+
+
+    // 상품 단건 조회 (productId)
+    Product findById(@Param("productId") long productId);
+
+
+    // 아우터, 상의, 하의 전체 상품 조회
+    List<Product> findByCategoryId(@Param("categoryId") long categoryId);
+
+
+    // 후드, 맨투맨 상품 조회
+    List<Product> findByDetailId(@Param("categoryDetailId") long categoryDetailId);
+
+
+    // 상품 등록
+    int insert (Product product);
+
+    // 상품 수정
+    int update (Product product);
+
+    // 상품 삭제
+    int deleteById(@Param("productId") long productId);
 
 
 }
