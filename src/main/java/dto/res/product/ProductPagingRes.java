b778378e6c5364f83de93b4941f1e08285b46140
bin <jeongbin@bagjeongbin-ui-MacBookPro.local>
2026-02-23 19:46:27 +0900

    add: product mapper, xml, service

---
+++ src/main/java/dto/res/product/ProductPagingRes.java
@@ -0,0 +1,20 @@
+package dto.res.product;
+
+import entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductPagingRes {
+    private int page; // 현재페이지
+    private int totalPages; // 전체 페이지 수
+    private int totalItemCount; // 전체 상품 갯수
+    private List<Product> items; // 현재 페이지 상품 리스트
+}
--- src/main/java/repository/mapper/CategoryMapper.java
+++ src/main/java/repository/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package repository.mapper;
 
 import entity.Category;
+import entity.Product;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -8,6 +9,8 @@ import java.util.List;
 
 @Mapper
 public interface CategoryMapper {
+
+
 
     // 카테고리 디테일 전체조회 (상의 하의 하의 안 카테고리)
     List<Category> findAll();
