525c029cc01f47b4898d8018b6a33937ac760fe9
bin <jeongbin@bagjeongbin-ui-MacBookPro.local>
2026-02-24 18:05:51 +0900

    add:keep

--- src/main/java/com/xmpy/demo/service/ProductService.java
+++ src/main/java/com/xmpy/demo/service/ProductService.java
@@ -1,9 +1,84 @@
-package com.xmpy.demo.service;
+package service;
 
 
+import dto.res.product.ProductPagingRes;
+import entity.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import repository.mapper.ProductMapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService  {
+
+    private final ProductMapper productMapper;
+
+    public ProductPagingRes getCategoryDetailPaging(
+            long categoryDetailId, int page) {
+
+        if (page < 1) {
+            page = 1;
+        }
+
+        int size = 15; // 한 페이지당 15개씩 나오게
+
+        // 총 갯수
+        int totalCount = productMapper.countByCategoryDetailId(categoryDetailId);
+
+        // 총 페이지 수
+        int totalPages = Math.max(1, (totalCount + size - 1) / size);
+
+        // offset 계산
+        int offset = (page -1) * size;
+
+        List<Product> list = productMapper.findByCategoryDetailId(categoryDetailId, size, offset);
+
+        return ProductPagingRes.builder()
+                .page(page)
+                .totalItemCount(totalCount)
+                .totalPages(totalPages)
+                .items(list)
+                .build();
+
+    }
+
+    // 전체 상품 조회
+    public List<Product> getAll() {
+        return productMapper.findAll();
+    }
+
+    // 상품 상의/ 하의 / 아우터 이런식으로 조회
+    public List<Product> getByCategoryId(long categoryId) {
+        return productMapper.findByCategoryId(categoryId);
+    }
+
+    // 상품 디테일로 조회
+    public List<Product> getByDetailId(long categoryDetailId) {
+        return productMapper.findByDetailId(categoryDetailId);
+    }
+
+    // 단건조회
+    public Product getById(long productId){
+        return productMapper.findById(productId);
+    }
+
+    // 상품 등록
+
+
+    // 상품 수정
+    public int update(Product product) {
+        product.setUpdatedAt(LocalDateTime.now());
+        return productMapper.update(product);
+    }
+
+    // 상품 삭제
+    public int delete(long productId) {
+        return productMapper.deleteById(productId);
+    }
+
+
 
 }
--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import entity.Product;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    // 전체 상품 조회
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+
 }
